870eb195b00c8901b66f5fe147df06f447043191
RK-Prime <rohankatyal74@gmail.com>
2024-04-04 20:09:05 +0530

    created numpy-practise.py modified Python-Basic.py

---
+++ Practise_program/Python-Basic.py
@@ -0,0 +1,30 @@
+
+# a = 10
+
+# print(f'normal division : {10/3}')
+# print(f'Remainder : {10%2}')
+# print(f'floor division : {10//3}')
+
+# print(f'Rohan{a}Katyal')
+
+# shorthand method
+# prompt and direct print method
+
+# a = print(input('Input first number : '))
+# b = print(input('Input second number : '))
+
+
+# a, b = [], []
+# n = int(input('Enter list size : '))
+
+# for i in range(n):
+#     a.append(i)
+#     b.append(i+1)
+
+# for i in range(0,n):
+#     a[i] = i
+#     b[i] = i+1
+
+# ^ will give error as list size is initially zero
+
+# print(a, b)
